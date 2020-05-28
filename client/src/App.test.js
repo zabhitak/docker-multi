98bb0cc075b26ac2ee7cf92c9361b3bc800d3ce1
zabhitak <zabhitak1292@gmail.com>
2020-05-28 21:32:41 +0530

    docker

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {});
+it('renders without crashing', () => {});
